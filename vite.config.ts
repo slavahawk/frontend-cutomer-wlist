7bfcd3f0fb5ddfa0df2f818d68123d2c322a4094
Slava <krylov_s10@mail.ru>
2025-02-05 14:52:57 +0700

    delete pwa

--- vite.config.ts
+++ vite.config.ts
@@ -4,44 +4,44 @@ import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 import { PrimeVueResolver } from "@primevue/auto-import-resolver";
 import Components from "unplugin-vue-components/vite";
-import { VitePWA } from "vite-plugin-pwa";
+// import { VitePWA } from "vite-plugin-pwa";
 import tailwindcss from "@tailwindcss/vite";
 
-const pwa = VitePWA({
-  manifest: {
-    name: "W-List - Винная карта",
-    short_name: "W-List",
-    description: "My awesome PWA application",
-    theme_color: "#ffffff",
-    background_color: "#ffffff",
-    display: "standalone",
-    scope: "/",
-    start_url: "/",
-    icons: [
-      {
-        src: "/android-chrome-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        src: "/android-chrome-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-      },
-      {
-        src: "/apple-touch-icon.png",
-        sizes: "180х180",
-        type: "image/png",
-      },
-    ],
-  },
-  workbox: {
-    // Настройки Workbox
-    // sourcemap: true,
-    globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
-  },
-  registerType: "autoUpdate",
-});
+// const pwa = VitePWA({
+//   manifest: {
+//     name: "W-List - Винная карта",
+//     short_name: "W-List",
+//     description: "My awesome PWA application",
+//     theme_color: "#ffffff",
+//     background_color: "#ffffff",
+//     display: "standalone",
+//     scope: "/",
+//     start_url: "/",
+//     icons: [
+//       {
+//         src: "/android-chrome-192x192.png",
+//         sizes: "192x192",
+//         type: "image/png",
+//       },
+//       {
+//         src: "/android-chrome-512x512.png",
+//         sizes: "512x512",
+//         type: "image/png",
+//       },
+//       {
+//         src: "/apple-touch-icon.png",
+//         sizes: "180х180",
+//         type: "image/png",
+//       },
+//     ],
+//   },
+//   workbox: {
+//     // Настройки Workbox
+//     // sourcemap: true,
+//     globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+//   },
+//   registerType: "autoUpdate",
+// });
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -63,7 +63,7 @@ export default defineConfig({
     Components({
       resolvers: [PrimeVueResolver()],
     }),
-    pwa,
+    // pwa,
   ],
   resolve: {
     alias: {
